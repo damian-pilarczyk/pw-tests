76727903a2fcee37fe88c4889f15fd88e1374495
damian-pilarczyk <damianpilarczyk93@gmail.com>
2024-01-01 16:54:45 +0100

    chore: improved sorting comparison

--- base/expects/order-expects.ts
+++ base/expects/order-expects.ts
@@ -3,49 +3,33 @@ import { Product } from '@models/products.interface';
 import { SortType } from '@models/sort-type.type';
 import { expect } from 'playwright/test';
 
-function expectLoHiOrder(products: Product[]): void {
-  for (let i = 1; i < products.length; i++) {
-    const price1 = priceToNumber(products[i - 1].price);
-    const price2 = priceToNumber(products[i].price);
-    expect(price2).toBeGreaterThanOrEqual(price1);
-  }
-}
-
-function expectHiLoOrder(products: Product[]): void {
-  for (let i = 1; i < products.length; i++) {
-    const price1 = priceToNumber(products[i - 1].price);
-    const price2 = priceToNumber(products[i].price);
-    expect(price2).toBeLessThanOrEqual(price1);
-  }
-}
-
-function expectAzOrder(products: Product[]): void {
-  for (let i = 1; i < products.length; i++) {
-    const compare = products[i].title.localeCompare(products[i - 1].title);
-    expect(compare).toBeGreaterThanOrEqual(0);
-  }
-}
-
-function expectZaOrder(products: Product[]): void {
-  for (let i = 1; i < products.length; i++) {
-    const compare = products[i].title.localeCompare(products[i - 1].title);
-    expect(compare).toBeLessThanOrEqual(0);
-  }
-}
-
-export function expectProductsOrder(products: Product[], sortType: SortType): void {
+function getExpected(products: Product[], sortType: SortType) {
   switch (sortType) {
     case 'az':
-      expectAzOrder(products);
-      break;
+      return products.map((x) => x.title).sort();
     case 'za':
-      expectZaOrder(products);
-      break;
+      return products
+        .map((x) => x.title)
+        .sort()
+        .reverse();
     case 'lohi':
-      expectLoHiOrder(products);
-      break;
+      return products
+        .map((x) => priceToNumber(x.price))
+        .sort((x, y) => x - y)
+        .map((x) => `${x}`);
     case 'hilo':
-      expectHiLoOrder(products);
-      break;
+      return products
+        .map((x) => priceToNumber(x.price))
+        .sort((x, y) => y - x)
+        .map((x) => `${x}`);
   }
+}
+
+export function expectProductsOrder(products: Product[], sortType: SortType) {
+  const expectedSort = getExpected(products, sortType);
+  const compare = ['lohi', 'hilo'].includes(sortType)
+    ? (product: Product, expected: string) => expect(priceToNumber(product.price)).toEqual(+expected)
+    : (product: Product, expected: string) => expect(product.title).toEqual(expected);
+
+  products.forEach((product, i) => compare(product, expectedSort[i]));
 }
