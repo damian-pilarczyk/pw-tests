bdd18cad6f58f327217eb471b62027c0ab2c968e
damian-pilarczyk <damianpilarczyk93@gmail.com>
2024-01-01 20:38:57 +0100

    chore: product sorting verification improved

--- base/expects/order-expects.ts
+++ base/expects/order-expects.ts
@@ -1,28 +1,23 @@
 import { priceToNumber } from '@helpers/convert';
 import { Product } from '@models/products.interface';
-import { SortType } from '@models/sort-type.type';
+import { NumberSortType, SortType, StringSortType } from '@models/sort-type.type';
 import { expect } from 'playwright/test';
 
 function getExpected(products: Product[], sortType: SortType) {
-  switch (sortType) {
-    case 'az':
-      return products.map((x) => x.title).sort();
-    case 'za':
-      return products
-        .map((x) => x.title)
-        .sort()
-        .reverse();
-    case 'lohi':
-      return products
-        .map((x) => priceToNumber(x.price))
-        .sort((x, y) => x - y)
-        .map((x) => `${x}`);
-    case 'hilo':
-      return products
-        .map((x) => priceToNumber(x.price))
-        .sort((x, y) => y - x)
-        .map((x) => `${x}`);
-  }
+  const titleOrder = (sort: StringSortType) => {
+    const order = products.map((x) => x.title).sort();
+    return sort === 'az' ? order : order.reverse();
+  };
+
+  const priceOrder = (sort: NumberSortType) =>
+    products
+      .map((x) => priceToNumber(x.price))
+      .sort((x, y) => (sort === 'lohi' ? x - y : y - x))
+      .map((x) => `${x}`);
+
+  const isTitleOrdering = (sortType: SortType): sortType is StringSortType => ['az', 'za'].includes(sortType);
+
+  return isTitleOrdering(sortType) ? titleOrder(sortType) : priceOrder(sortType);
 }
 
 export function expectProductsOrder(products: Product[], sortType: SortType) {
--- models/sort-type.type.ts
+++ models/sort-type.type.ts
@@ -1,1 +1,3 @@
-export type SortType = 'az' | 'za' | 'lohi' | 'hilo';
+export type NumberSortType = 'lohi' | 'hilo';
+export type StringSortType = 'az' | 'za';
+export type SortType = NumberSortType | StringSortType;
